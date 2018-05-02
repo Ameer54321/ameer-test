f2aba5a3d9d0a544661faa940f98ae623fb227ca
Rodney T. Ncane <thembinkosiklein@users.noreply.github.com>
2018-05-02 09:21:06 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
 
 const config = {
+	host: 'localhost',
 	port: process.env.PORT || 8000,
 	key: 'BbZJjyoXAdr8BUZuiKKARWimKfrSmQ6fv8kZ7OFfc',
 	database: {
@@ -19,9 +20,13 @@ const config = {
 		}
 	},
 	email: {
+		quote_url: 'http://dashbundle.co.za/emails/', 
 		mandrill: {
 			key: 'njqRVZ3J9J3psHDoFjnTLQ'
 		}
+	},
+	support: {
+		email: 'support@cloudlogic.co.za'
 	}
 }
 
